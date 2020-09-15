1ac694089a81a1aa494d1afdb0a0df9c5e9f7e60
javiperdomog <javiperdomog@gmail.com>
2020-09-15 12:36:24 -0500

    Cambio de ioutilities

--- io_utilities.py
+++ io_utilities.py
@@ -1,5 +1,13 @@
 # io_utilities.py
 
 def read_vanilla(filepath):
-    pass
-    return data
+    filepath = './data/iris.data'
+
+    with open(filepath,'r') as fp:
+        data = fp.read()
+
+    data_lines = data.split('\n')
+
+    data_final = [f.split(',') for f in data_lines]
+
+    return data_final
